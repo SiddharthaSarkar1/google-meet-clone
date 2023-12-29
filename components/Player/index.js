64d1012bff9c0012c0c05ee6552a03785bb02b1a
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-12-30 02:19:55 +0530

    styling  players

--- components/Player/index.js
+++ components/Player/index.js
@@ -1,14 +1,21 @@
-import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player";
+import cx from "classnames";
+
+import styles from "@/components/Player/index.module.css";
 
 const Player = (props) => {
-
-    const { playerID, url, muted, playing } = props;
+  const { url, muted, playing, isActive } = props;
 
   return (
-    <div>
-      <ReactPlayer key={playerID} url={url} muted={muted} playing={playing} />
+    <div
+      className={cx(styles.playerContainer, {
+        [styles.notActive]: !isActive,
+        [styles.active]: isActive,
+      })}
+    >
+      <ReactPlayer url={url} muted={muted} playing={playing} height='100%' width='100%' />
     </div>
-  )
-}
+  );
+};
 
 export default Player;
--- hooks/usePlayer.js
+++ hooks/usePlayer.js
@@ -1,11 +1,20 @@
+import { cloneDeep } from "lodash";
 import { useState } from "react";
 
-const usePlayer = () => {
+const usePlayer = (myId) => {
   const [players, setPlayers] = useState({});
+  const playersCopy = cloneDeep(players);
+
+  const playerHighlighted = playersCopy[myId];
+  delete playersCopy[myId];
+
+  const nonHighlightedPlayers = playersCopy;
 
   return {
     players,
     setPlayers,
+    playerHighlighted,
+    nonHighlightedPlayers,
   };
 };
 
